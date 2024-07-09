ae93987c87758d7a5412046500c71ec628ff10e5
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2024-07-09 12:04:34 -0300

    delete service

--- app/src/main/java/com/projectmanager/config/AppConfig.java
+++ app/src/main/java/com/projectmanager/config/AppConfig.java
@@ -3,11 +3,8 @@ package com.projectmanager.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.projectmanager.service.DateSortingStrategy;
 import com.projectmanager.service.GitService;
-import com.projectmanager.service.GithubService;
-import com.projectmanager.service.GitlabService;
+
 
 import com.projectmanager.service.TaskSortingStrategy;
 
@@ -25,36 +22,6 @@ public class AppConfig {
     public void init() {
         gitServiceType = GitServiceType.valueOf(gitServiceTypeString.toUpperCase());
     }
-
-    @Bean(name = "dateSortingStrategy")
-    public DateSortingStrategy dateSortingStrategy() {
-        return new DateSortingStrategy();
-    }
-
-    @Bean("GitlabService")
-    public GitService gitlabService() {
-        return new GitlabService();
-    }
-
-    @Bean("GitHubService")
-    public GitService gitHubService() {
-        return new GithubService();
-    }
-
-    // private TaskSortingStrategy createSortingStrategy(String beanName) {
-    //     String gitClass = Global.GitClass;
-
-    //     if ("GithubService2".equals(gitClass)) {
-    //         // Injeta a estratégia de ordenação por data para GitlabService
-    //         return new DateSortingStrategy();
-    //     } else if ("GitlabService".equals(gitClass)) {
-    //         // Injeta a estratégia de ordenação por prioridade para GithubService2 (exemplo
-    //         // fictício)
-    //         return new PrioritySortingStrategy();
-    //     } else {
-    //         throw new IllegalStateException("Configuração de classe Git não suportada: " + gitClass);
-    //     }
-    // }
 
     @Bean(name = "sortingStrategy")
     public TaskSortingStrategy createSortingStrategy() {
